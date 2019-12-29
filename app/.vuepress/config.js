555e8987a603508307e2a459a5c1eeb83577b18c
zhanglei <lzhang01@kaikeba.com>
2019-12-30 01:17:14 +0800

    新编译
    更牛逼

--- app/.vuepress/config.js
+++ app/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
     description: "我不想给自己打标签，也不想给自己划界线。",
     // theme: 'vuepress-theme-xx',
     // theme: '@vuepress/vue',
-    dest: '/Applications/jiuyangzhenjing.github.io/public',
+    dest: '/Users/lz/GitHub/jiuyangzhenjing.github.io/public',
     themeConfig: {
         nav: [{
             text: '技术',
